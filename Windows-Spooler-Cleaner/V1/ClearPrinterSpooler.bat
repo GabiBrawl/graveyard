f592231994cb1a0e242ae45fe92528d9c98e198b
GabiBrawl <gabiya219@gmail.com>
2026-02-24 15:33:31 +0000

    Archive Windows-Spooler-Cleaner (code + releases) from https://github.com/GabiBrawl/Windows-Spooler-Cleaner

---
+++ Windows-Spooler-Cleaner/V1/ClearPrinterSpooler.bat
@@ -0,0 +1,35 @@
+@echo off
+:chk_admin
+cls
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+
+:clr_spool
+cls
+echo Detected Admin privileges.
+net stop spooler
+cd %systemroot%\system32\spool\PRINTERS
+del /f /s *.SHD
+del /f /s *.SPL
+net start spooler
+pause> nul | set /p "=Done! Press any key to exit..." 
+exit
